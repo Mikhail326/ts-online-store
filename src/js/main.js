dfa719ecaa8c5e5a2ea0f53230cfc7d136df6a33
Mikhail326 <misha.stelmakh1995@gmail.com>
2023-07-06 15:33:12 +0300

    added header--advantages

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.header-slider').slick({
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 1,
+    centerMode:true,
+    arrows: false,
+    dots: true,
+  })
+})
